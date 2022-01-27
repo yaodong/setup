e2519183b99bd289b48af2e2b7c7c76c5091a5e0
Yaodong Zhao <git@yaodong.dev>
2022-01-27 00:58:41 -0800

    enable web mode and disable format-all for it

--- config.el
+++ config.el
@@ -53,3 +53,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; disbale tidy because it mess up erb templates
+(setq-hook! 'web-mode-hook +format-with :none)
