200a4d22277802720af3064ff98dc4d5c2ec6870
Yaodong Zhao <git@yaodong.dev>
2022-02-14 15:33:49 -0800

    set font

--- config.el
+++ config.el
@@ -21,7 +21,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Hack" :size 13))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 14 :weight 'semi-light)
+      doom-big-font-increment 2)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -74,3 +75,7 @@
         web-mode-css-indent-offset 2
         web-mode-code-indent-offset 2)
   )
+
+(use-package treemacs
+  :config
+  (treemacs-follow-mode t))
