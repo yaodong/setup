f576e13d37dbba1721d41b04dff29932f129d9f4
Yaodong Zhao <git@yaodong.dev>
2024-01-07 13:30:03 -0800

    Use rubocop instead of standardrb

--- modules/custom/ruby/config.el
+++ modules/custom/ruby/config.el
@@ -3,7 +3,7 @@
 
 (defun custom|ruby-setup-flycheck ()
   "Use ruby-standard as the default checker for ruby-mode."
-  (flycheck-select-checker 'ruby-standard))
+  (flycheck-select-checker 'ruby-rubocop))
 
 (use-package! ruby-mode
   :config
@@ -12,11 +12,12 @@
 ;; Explicitly set the ruby and erb formatters
 (use-package! apheleia
   :config
-  ;; https://github.com/standardrb/standard
-  (add-to-list 'apheleia-formatters
-               '(ruby-standard . ("standardrb" "--stdin" filepath "--fix" "--stderr" "--format" "quiet" "--fail-level" "fatal")))
+  ;; ;; https://github.com/standardrb/standard
+  ;; (add-to-list 'apheleia-formatters
+  ;;              '(ruby-standard . ("standardrb" "--stdin" filepath "--fix" "--stderr" "--format" "quiet" "--fail-level" "fatal")))
   (add-to-list 'apheleia-mode-alist
-               '(ruby-mode . ruby-standard))
+               '(ruby-mode . rubocop))
+
   ;; https://github.com/nebulab/erb-formatter
   (add-to-list 'apheleia-formatters
                '(erb-format . ("erb-format" "--stdin" "--print-width" "120" "--stdin-filename" filepath)))
