ea5eefbf14bd91e2084c15c082b25482b349d531
Yaodong Zhao <git@yaodong.dev>
2024-12-15 21:30:54 -0800

    telescope does not work on one of my machine

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
