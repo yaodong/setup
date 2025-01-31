c591ed1b2e502cc1160036e71e38a6177fd30e07
Yaodong Zhao <git@yaodong.dev>
2025-01-30 22:14:39 -0800

    Add ideavimrc and update nvim config

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,4 +3,85 @@ return {
     "echasnovski/mini.comment",
     event = "VeryLazy",
   },
+
+  -- programming language parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "ruby",
+        "embedded_template",
+      },
+    },
+  },
+
+  -- install lsp servers, linters, and formatters
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "pyright",
+        "erb-formatter",
+      },
+    },
+  },
+
+  -- configurate nvim-lspconfig base on then mason configuration
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "ruby_lsp",
+        "rubocop",
+      },
+    },
+  },
+
+  -- configurate nvim to use lsp servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          enabled = true,
+        },
+        rubocop = {
+          enabled = true,
+        },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ruby = { "rubocop" },
+        eruby = { "erb_format" },
+      },
+      formatters = {
+        erb_format = {
+          prepend_args = { "--print-width", "120" },
+        },
+      },
+    },
+  },
+
+  {
+    "nvim-neotest/neotest",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-neotest/neotest-python",
+      "zidhuss/neotest-minitest",
+    },
+    opts = function()
+      return {
+        adapters = {
+          require("neotest-python"),
+          require("neotest-minitest"),
+        },
+      }
+    end,
+  },
 }
