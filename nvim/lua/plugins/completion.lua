44e4345e133a4f1c7334a248674606910b375fef
Yaodong Zhao <git@yaodong.dev>
2025-02-07 14:28:57 -0800

    nvim - use enter to select completion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -6,7 +6,7 @@ return {
     },
     event = "InsertEnter",
     opts = {
-      keymap = { preset = "default" },
+      keymap = { preset = "enter" },
       appearance = {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = "mono",
