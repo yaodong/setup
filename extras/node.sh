4b18e95b6978244e1f603c7b0e68e0fdd6492647
Yaodong Zhao <git@yaodong.dev>
2025-03-06 14:14:55 -0800

    Add more extras

--- extras/node.sh
+++ extras/node.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-
 brew tap oven-sh/bun
 brew install oven-sh/bun/bun
-brew install nvm
 
 if [ ! -d "$HOME/.nvm" ]; then
   git clone --depth=1 https://github.com/nvm-sh/nvm.git ~/.nvm
@@ -13,5 +11,5 @@ source ~/.nvm/nvm.sh
 nvm install --lts
 nvm use --lts
 
-
 echo "Node $(node --version) installed"
+
