f4c2c5f168270e1913c255d008003c3ef6e4d4f4
Yaodong Zhao <git@yaodong.dev>
2025-03-10 16:29:11 -0700

    update mac key setup

--- extras/mac.sh
+++ extras/mac.sh
@@ -23,6 +23,12 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Dock, Input, Spaces, and Keyboard
 #
 
+# Use the Fn key to switch between keyboard layouts for writing in other languages
+defaults write com.apple.HIToolbox AppleFnUsageType -int "1"
+
+# Repeats the key as long as it is held down.
+defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
+
 # Set the icon size of Dock items to 36 pixels
 defaults write com.apple.dock tilesize -int 36
 
