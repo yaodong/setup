01b7e9dfa17eb424d1ce766d8f9990bd2eeb53d6
Yaodong Zhao <git@yaodong.dev>
2025-04-28 15:01:38 -0700

    Update mac setup

--- extras/mac.sh
+++ extras/mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# General UI/UX  
+# General UI/UX
 #
 
 # Disable automatic capitalization as it’s annoying when typing code
@@ -35,18 +35,6 @@ defaults write com.apple.dock tilesize -int 36
 # Lock the Dock size
 defaults write com.apple.Dock size-immutable -bool yes
 
-# Change minimize/maximize window effect
-defaults write com.apple.dock mineffect -string "scale"
-
-# Minimize windows into their application’s icon
-defaults write com.apple.dock minimize-to-application -bool true
-
-# Don’t animate opening applications from the Dock
-defaults write com.apple.dock launchanim -bool false
-
-# Don’t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
@@ -56,5 +44,7 @@ defaults write com.apple.dock show-recents -bool false
 # Display the input menu in the menu bar
 defaults write com.apple.TextInputMenu visible -bool true
 
+# Restart Dock
+killall Dock
 
-echo "Done. Note that some of these changes require a logout/restart to take effect."
+echo "Done"
