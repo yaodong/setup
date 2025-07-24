52abb53dea5d9a03dd47f9937420d02bc158d7ec
Yaodong Zhao <git@yaodong.dev>
2025-07-24 13:26:11 -0700

    Update space items in sketchybar

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set $NAME background.drawing=on
+else
+    sketchybar --set $NAME background.drawing=off
+fi
