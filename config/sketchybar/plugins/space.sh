75fade9f3cbf5485c1f7c313b7ec69af71b57fe6
Yaodong Zhao <git@yaodong.dev>
2025-07-29 21:14:32 -0700

    Replace aerospace with yabai due to performance issue

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
-else
-    sketchybar --set $NAME background.drawing=off
-fi
+# The $SELECTED variable is available for space components and indicates if
+# the space invoking this script (with name: $NAME) is currently selected:
+# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
+
+sketchybar --set "$NAME" background.drawing="$SELECTED"
